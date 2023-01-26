c1d0e2295ec69dd92f2901b6a2812ef091ff9560
Daniel Durand <danydurand@gmail.com>
2023-01-25 21:55:10 -0400

    Data Access, Models, and Utility tiers were created

---
+++ BulkyBook.Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models;
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [DisplayName("Display Order")]
+    [Range(1, 50, ErrorMessage = "Display Order must be between 1 and 50.")]
+    public int DisplayOrder { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
